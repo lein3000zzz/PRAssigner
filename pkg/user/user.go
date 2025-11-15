5b11c2d264794744c18d340bd3a1b11319abf45e
lein3000zzz <146432991+lein3000zzz@users.noreply.github.com>
2025-11-15 13:40:18 +0300

    remove unused repo methods and clear the interface

--- pkg/user/user.go
+++ pkg/user/user.go
@@ -18,18 +18,6 @@ type User struct {
 	UpdatedAt time.Time
 }
 
-// UsersRepo - вообще можно было бы оставить только SetIsActive, но все остальное было сделано под предлогом зависимости от самой сущности
-// user в других репах, поэтому я решил и сюда это добавить для полноты реализации и упрощения генерации данных для тестов.
-// Почему не оставил методы здесь и не импортировал юзеррепу с этими методами туда?
-// Ответ: там требуется несколько действий, которые для консистентности должны быть совершены в рамках одной транзакции, что потребовало
-// бы передавать *gorm.DB. Добавлять *gorm.DB в интерфейс мне показалось преступлением, поэтому я сделал фактически дубликат кода
-// (хотя я его сделал только для себя, поскольку он нигде не используется (если успею по дедлайнам, то TODO)).
-// RUG, получается: Repeat Until Good
 type UsersRepo interface {
 	SetIsActive(userID string, isActive bool) (*User, error)
-	GetByID(userID string) (*User, error)
-	UpsertUsers(teamName string, users []*User) error
-	ListActiveByTeamExcept(teamName string, excludeIDs []string, limit int) ([]*User, error)
-	ListByTeam(teamName string) ([]*User, error)
-	ListReviewPRIDs(userID string) ([]string, error)
 }
--- pkg/user/user_pg_repo.go
+++ pkg/user/user_pg_repo.go
@@ -23,41 +23,6 @@ func NewUsersRepoPg(logger *zap.SugaredLogger, db *gorm.DB) *UsersRepoPg {
 	}
 }
 
-func (repo *UsersRepoPg) GetByID(userID string) (*User, error) {
-	repo.logger.Debugw("getByID()", "userID", userID)
-
-	var user User
-	if err := repo.db.First(&user, "user_id = ?", userID).Error; err != nil {
-		repo.logger.Errorw("error getting user by id", "userID", userID)
-		return nil, err
-	}
-
-	repo.logger.Debugw("got user by id", "userID", userID)
-	return &user, nil
-}
-
-func (repo *UsersRepoPg) UpsertUsers(teamName string, users []*User) error {
-	repo.logger.Debugw("upsertUsers()", "teamName", teamName)
-
-	if len(users) == 0 {
-		repo.logger.Debugw("no users to update", "teamName", teamName)
-		return nil
-	}
-
-	usersCopy := make([]*User, len(users))
-	for i, user := range users {
-		newU := *user
-		newU.TeamName = teamName
-		usersCopy[i] = &newU
-	}
-
-	repo.logger.Debugw("upserting users", "teamName", teamName, "users", usersCopy)
-	return repo.db.Clauses(clause.OnConflict{
-		Columns:   []clause.Column{{Name: "user_id"}},
-		DoUpdates: clause.AssignmentColumns([]string{"username", "team_name", "is_active"}),
-	}).Create(&usersCopy).Error
-}
-
 func (repo *UsersRepoPg) SetIsActive(userID string, isActive bool) (*User, error) {
 	repo.logger.Debugw("setIsActive()", "userID", userID, "isActive", isActive)
 
@@ -80,59 +45,4 @@ func (repo *UsersRepoPg) SetIsActive(userID string, isActive bool) (*User, error
 
 	repo.logger.Debugw("got user by id", "userID", userID)
 	return &user, nil
-}
-
-func (repo *UsersRepoPg) ListActiveByTeamExcept(teamName string, excludeIDs []string, limit int) ([]*User, error) {
-	repo.logger.Debugw("listActiveByTeamExcept()", "teamName", teamName)
-
-	var users []*User
-	query := repo.db.Where("team_name = ? AND is_active = true", teamName)
-
-	if len(excludeIDs) > 0 {
-		query = query.Where("user_id NOT IN ?", excludeIDs)
-	}
-	if limit > 0 {
-		query = query.Limit(limit)
-	}
-
-	if err := query.Order("user_id").Find(&users).Error; err != nil {
-		repo.logger.Errorw("error listing active users", "error", err)
-		return nil, err
-	}
-
-	repo.logger.Debugw("got active users", "users", users)
-	return users, nil
-}
-
-func (repo *UsersRepoPg) ListByTeam(teamName string) ([]*User, error) {
-	repo.logger.Debugw("listByTeam()", "teamName", teamName)
-
-	var users []*User
-	if err := repo.db.
-		Where("team_name = ?", teamName).
-		Order("user_id").
-		Find(&users).Error; err != nil {
-		repo.logger.Errorw("error listing users", "error", err)
-		return nil, err
-	}
-
-	repo.logger.Debugw("got users by team", "teamName", teamName, "users", users)
-	return users, nil
-}
-
-func (repo *UsersRepoPg) ListReviewPRIDs(userID string) ([]string, error) {
-	repo.logger.Debugw("listReviewPRIDs()", "userID", userID)
-
-	var ids []string
-	err := repo.db.
-		Model(&PRReviewer{}).
-		Where("user_id = ?", userID).
-		Pluck("pull_request_id", &ids).Error
-	if err != nil {
-		repo.logger.Errorw("error listing PR reviewers", "error", err)
-		return nil, err
-	}
-
-	repo.logger.Debugw("got PR reviewers", "userID", userID, "pull_requestIds", ids)
-	return ids, nil
 }
